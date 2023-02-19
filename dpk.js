5bfaacaaa2a60fe3a71862f9252aaeb95946c933
allmysmarts <rockid332@gmail.com>
2023-02-19 18:16:31 +0900

    Refactor minor

--- dpk.js
+++ dpk.js
@@ -7,12 +7,13 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
+  const hash = crypto.createHash("sha3-512");
 
   if (!event) {
     return TRIVIAL_PARTITION_KEY;
   }
   if (!event.partitionKey) {
-    return crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
+    return hash.update(JSON.stringify(event)).digest("hex");
   }
   const partitionKey = typeof event.partitionKey === "string" ? 
     event.partitionKey : 
@@ -21,5 +22,5 @@ exports.deterministicPartitionKey = (event) => {
   if (partitionKey.length <= MAX_PARTITION_KEY_LENGTH) {
     return partitionKey;
   }
-  return crypto.createHash("sha3-512").update(partitionKey).digest("hex");
+  return hash.update(partitionKey).digest("hex");
 };
